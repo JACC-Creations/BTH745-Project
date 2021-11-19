479d913763caeca201b15cd55594eced5ccd1633
JACC-Creations <JACC.Create@gmail.com>
2021-11-19 12:33:39 -0500

    added enough functionality for usability testing (error checking, mock-login feature, etc)

--- JS/index.js
+++ JS/index.js
@@ -9,38 +9,8 @@ $(".multi-item-carousel .carousel-item").each(function () {
   prev.children(":nth-last-child(2)").clone().prependTo($(this));
 });
 
-document.getElementById("b1").addEventListener("click", function () {
-  const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: "btn btn-success",
-      cancelButton: "btn btn-danger",
-    },
-    buttonsStyling: true,
-  });
-
-  swalWithBootstrapButtons
-    .fire({
-      title: "Are you sure you want to make this transfer?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Yes",
-      confirmButtonColor: "#3da614",
-      cancelButtonText: "No",
-      cancelButtonColor: "#d33",
-      reverseButtons: false,
-    })
-    .then((result) => {
-      if (result.isConfirmed) {
-        swalWithBootstrapButtons.fire(
-          "Success!",
-          "Your new Balance: 250 ETH",
-          "success"
-        );
-      } else if (
-        /* Read more about handling dismissals below */
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-        swalWithBootstrapButtons.fire("Cancelled", "Transaction cancelled");
-      }
-    });
-});
+var login = false;
+var accountBalance = 1000;
+var username = "jared.cotton";
+var password = "charles12";
+var address = "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"
